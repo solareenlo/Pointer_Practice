a5f952cd4fd884dbb70993481c25e47a75ad9645
solareenlo <solareenlo@gmail.com>
2018-09-30 09:24:34 +0900

    ポインタと配列の関係性

---
+++ address_indirection/pointer_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    char str[] = "Hello World";
+
+    // 配列の中身、配列の先頭の値、配列の中身のアドレス、配列の先頭の値のアドレス、配列のアドレス
+    printf("%s, %c, %x, %x, %x\n", str, str[0], &str, &str[0], str);
+
+    return 0;
+}
